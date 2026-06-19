46566c51cc3a383046150a6131ca488207a0c574
ahamedshakir02 <ahamedshakir02@gmail.com>
2026-06-20 00:33:02 +0530

    fix: add Gallery route to App component for improved navigation

--- src/App.jsx
+++ src/App.jsx
@@ -13,6 +13,7 @@ import Home from './pages/Home'
 import About from './pages/About'
 import Services from './pages/Services'
 import Projects from './pages/Projects'
+import Gallery from './pages/Gallery'
 import Process from './pages/Process'
 import Contact from './pages/Contact'
 import NotFound from './pages/NotFound'
@@ -48,6 +49,7 @@ export default function App() {
           <Route path="/about" element={<About />} />
           <Route path="/services" element={<Services />} />
           <Route path="/projects" element={<Projects />} />
+          <Route path="/gallery" element={<Gallery />} />
           <Route path="/process" element={<Process />} />
           <Route path="/contact" element={<Contact />} />
           <Route path="*" element={<NotFound />} />
